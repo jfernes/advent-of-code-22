b8ea0b40becdac64875025ee375ac9db47c99f25
jfernes <jfernesc@gmail.com>
2022-12-11 20:06:50 +0100

    day 3 part 1

---
+++ day3_p1.py
@@ -0,0 +1,21 @@
+def get_ascii(c):
+    if ord(c) > 96:
+        return ord(c) - 96
+    else:
+        return ord(c) - 64 + 26
+
+if __name__ == "__main__":
+    prio_sum = 0
+    file = open("day3input.txt", "r")
+    for line in file:
+        used = []
+        line = line.strip()
+        rs1 = line[:int(len(line)/2)]
+        rs2 = line[int(len(line)/2):]
+        for i in range(0, int(len(rs1))):
+            for j in range(0, int(len(rs2))):
+                if rs1[i] == rs2[j] and used.count(rs1[i]) == 0:
+                    prio_sum += get_ascii(rs1[i])
+                    used.append(rs1[i])
+
+    print(prio_sum)
